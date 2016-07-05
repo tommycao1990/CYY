36681c0f9c937e451ad89cdbeaacdc78f80a7e0b
Tommy <820572862@qq.com>
2016-07-05 15:21:00 +0800

    初次提交
    空项目

---
+++ CyyProduct/CyyProduct/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CyyProduct
+//
+//  Created by Tommy on 16/7/5.
+//  Copyright © 2016年 Tommy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CyyProduct/CyyProduct/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CyyProduct
+//
+//  Created by Tommy on 16/7/5.
+//  Copyright © 2016年 Tommy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
